211a0b57f584f1b498912abc4462663418f3b315
vladimirshkarin <vladimir.shkarin@gmail.com>
2026-08-21 14:27:27 +0300

    remove error tracking check

--- task_manager/views.py
+++ task_manager/views.py
@@ -3,7 +3,3 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = "index.html"
-
-
-def trigger_error(request):
-    raise RuntimeError("Bugsink production integration test")
